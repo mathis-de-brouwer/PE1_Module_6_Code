762c1b33edcb6e1ef0d322168bc68d152cadc81e
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-25 10:13:50 +0100

    oef21

---
+++ oef/oef21.mjs
@@ -0,0 +1,7 @@
+
+let ingredients = [`ing0`, `ing1`, `ing2`, `ing3`, `ing4`, `ing5`, `ing6`];
+console.log(`mijn ingredientenlijst: \n`);
+
+for(let i = 0; i < ingredients.length; i++){
+    console.log(ingredients[i]);
+}
