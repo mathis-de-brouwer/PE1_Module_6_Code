736107e40de11b3a869af0e733eba135e571b9bb
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-25 11:03:14 +0100

    oef 22

--- oef/oef22.mjs
+++ oef/oef22.mjs
@@ -1,4 +1,20 @@
+// Begin van boilerplate dat je even mag vergeten
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+import { constants } from 'node:buffer';
+import { parse } from 'node:path';
+const userInput = readline.createInterface({input, output});
+// Einde van boilerplate dat je even mag vergeten
+//h2
 
 
+let names = [];
+let name;
 
+do{
+    name = (await userInput.question(`give name: `));
+    names.push(name);
+}while(name != ``)
+
+console.log(names);
 process.exit();
